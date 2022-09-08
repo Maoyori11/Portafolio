a8c94d28b70debd13220e7daae35d06b5e8966ab
Maoyori11 <maoyori11@gmail.com>
2022-09-08 13:26:44 -0500

    Portafolio_Academlo

---
+++ index.js
@@ -0,0 +1,167 @@
+window.sr = ScrollReveal();
+// window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.header', { origin: 'top', distance: '120px', delay: 200 })
+sr.reveal('.home__title', { delay: 1000 })
+
+  sr.reveal('.nav-menu', {
+    duration: 3000,
+    origin: 'botton',
+  });
+
+  sr.reveal('.logo-header', {
+    duration: 2000,
+    origin: 'botton',
+    distance: '-100px'
+  });
+
+  sr.reveal('.img_desktop', {
+    duration: 6000,
+        rotate: {
+            x: 1,
+            y: 45,
+            z: 0,
+        }
+  });
+
+  sr.reveal('.img_desktop', {
+    duration: 4000,
+    origin: 'top',
+    distance: '-100px'
+  });
+
+  sr.reveal('.section-company', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px'
+  });
+
+  sr.reveal('.box_company', {
+    duration: 2000,
+    origin: 'botton',
+    distance: '-100px'
+  });
+
+  sr.reveal('.title-Soy', {
+    duration: 2000,
+    origin: 'botton',
+    distance: '-100px'
+  });
+
+  sr.reveal('.line_destok', {
+    duration: 3500,
+    origin: 'botton',
+    distance: '-100px'
+  });
+
+  sr.reveal('.line_destok', {
+    duration: 4000,
+        rotate: {
+            x: 0,
+            y: 1,
+            z: 45,
+        }
+  });
+
+  sr.reveal('.title-Mauro', {
+    duration: 2500,
+    origin: 'botton',
+    distance: '-100px'
+  });
+
+  sr.reveal('.presentataion_me', {
+    duration: 3000,
+    origin: 'botton',
+    distance: '-100px'
+  });
+  
+  sr.reveal('.experiencia', {
+    duration: 3500,
+    origin: 'botton',
+    distance: '-100px'
+  });
+
+  sr.reveal('.box-redes', {
+    duration: 4000,
+    origin: 'botton',
+    distance: '-100px'
+  });
+
+  sr.reveal('.box-redes', {
+    duration: 5000,
+        rotate: {
+            x: 0,
+            y: 1,
+            z: -45,
+        }
+  });
+
+  sr.reveal('.About', {
+    duration: 2500,
+    origin: 'left',
+    distance: '400px'
+  });
+
+  sr.reveal('.Servicios', {
+    duration: 2500,
+    origin: 'botton',
+    distance: '500px'
+  });
+
+  sr.reveal('.swiper-wrapper', {
+    duration: 3000,
+    origin: 'top',
+    distance: '200px'
+  });
+
+  sr.reveal('.habilidades', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px'
+  });
+
+  sr.reveal('.skill', {
+    duration: 3000,
+    origin: 'botton',
+    distance: '-200px'
+  });
+
+  sr.reveal('.mi-portfalio-titulo', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-200px'
+  });
+
+
+  sr.reveal('.contact-title', {
+    duration: 3000,
+    origin: 'botton',
+    distance: '500px'
+  });
+
+  sr.reveal('.box-contac1', {
+    duration: 3000,
+    origin: 'botton',
+    distance: '200px'
+  });
+
+  sr.reveal('.box-contac2', {
+    duration: 2500,
+    origin: 'top',
+    distance: '200px'
+  });
+
+  sr.reveal('.footer', {
+    duration: 2500,
+    origin: 'top',
+    distance: '-100px'
+  });
+
+  sr.reveal('.iconos-redes', {
+    duration: 3000,
+    origin: 'top',
+    distance: '50px',
+    opacity: 100,
+
+  });
+
